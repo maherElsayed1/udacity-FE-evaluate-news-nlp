a5089ca09835b5dcb1793254df8aa12db33f8f67
maher elsayed <maher.elsayed.dev@gmail.com>
2020-10-10 00:21:37 +0200

    test: add unit testing using jest

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -28,6 +28,7 @@ const postData = async (url = "", data = {}) => {
     const response = await fetch(url, {
         method: "POST",
         credentials: "same-origin",
+        mode: 'cors',
         headers: {
             "Content-Type": "application/json",
         },
