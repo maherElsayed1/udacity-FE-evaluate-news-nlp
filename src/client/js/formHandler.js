a1b18b207a664267712c3f23a85b8cc7be5f17cd
maher elsayed <maher.elsayed.dev@gmail.com>
2020-10-09 16:43:44 +0200

    Update formHandler function()

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,18 +1,22 @@
-import fetch from "node-fetch";
-
 function handleSubmit(event) {
     event.preventDefault();
 
     // check what text was put into the form field
     let formText = document.getElementById("name").value;
-    checkForName(formText);
 
     console.log("::: Form Submitted :::");
-    fetch("http://localhost:8080/test")
-        .then((res) => res.json())
-        .then(function (res) {
-            document.getElementById("results").innerHTML = res.message;
+
+    if (Client.validURL(formText)) {
+        postData("http://localhost:8080/myMeaningCloud", { url: formText }).then(function (res) {
+            document.getElementById("polarity").innerHTML = `Polarity: ${res.score_tag}`;
+            document.getElementById("agreement").innerHTML = `Agreement: ${res.agreement}`;
+            document.getElementById("subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
+            document.getElementById("confidence").innerHTML = `Confidence: ${res.confidence}`;
+            document.getElementById("irony").innerHTML = `Irony: ${res.irony}`;
         });
+    } else {
+        alert("Please Enter A Valid URL");
+    }
 }
 
 // Function to post Data
